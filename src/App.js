43f79da52f86c60137381cf7350d36ccb7397eac
Magdalena Buzikiewicz <buzu@op.pl>
2020-09-27 18:11:38 +0200

    Board component skeleton.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import './App.css';
+import React, { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+import Board from './components/Board'
 
 function App() {
   const [score, setScore] = useState(0);
   return (
     <div className="App">
       <Header score={score}/>
+      <Board />
     </div>
   );
 }
