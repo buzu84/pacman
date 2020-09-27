2f1446090795f505b7df38e99618182375394496
Magdalena Buzikiewicz <buzu@op.pl>
2020-09-27 19:05:41 +0200

    SVG used as React Component, positioning Pacman component through state.

--- src/components/Pacman/Pacman.js
+++ src/components/Pacman/Pacman.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 import './style.css'
+import { ReactComponent as PacmanSvg } from './pacman.svg'
 
 class Pacman extends Component {
+  state = {
+    direction: 'right',
+    position: {
+      top: 0,
+      left: 0
+    }
+  }
+
   render () {
     return (
-      <div className="pacman">pacman</div>
+      <div
+      className="pacman"
+      style={this.state.position}
+      >
+        <PacmanSvg />
+      </div>
     )
   }
+}
+
+Pacman.defaultProps = {
+  // TODO move to config
+  step: 50,
+  size: 50,
+  border: 20,
+  topScoreBoardHeight: 50
 }
 
 export default Pacman;
