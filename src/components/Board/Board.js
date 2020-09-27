8a23e98ead582a3459ea3e5aa9e594e82fc16aff
Magdalena Buzikiewicz <buzu@op.pl>
2020-09-27 18:38:21 +0200

    Pacman component skeleton and pacman svg.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import './style.css'
+import Pacman from '../Pacman'
 
 class Board extends Component {
   render () {
     return (
-      <div className="board">Hello!</div>
+      <div className="board">
+        <Pacman />
+      </div>
     )
   }
 }
---
+++ src/components/Pacman/Pacman.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class Pacman extends Component {
+  render () {
+    return (
+      <div className="pacman">pacman</div>
+    )
+  }
+}
+
+export default Pacman;
