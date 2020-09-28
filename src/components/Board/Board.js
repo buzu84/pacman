d448e44db89be9edf7d38bfea7d1a084087f2793
Magdalena Buzikiewicz <buzu@op.pl>
2020-09-28 09:07:17 +0200

    Created logic to check up how much food dots can be rendered depending on window size.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,6 +5,23 @@ import Ghost from '../Ghost'
 import Food from '../Food'
 
 class Board extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.amountOfFood = (
+      (window.innerWidth - this.props.foodSize)
+      * (window.innerHeight - this.props.topScoreBoardHeight)
+    ) / (this.props.foodSize * this.props.foodSize) - 1;
+
+    console.log("ile zmiesci sie jedzenia: ", this.amountOfFood);
+    console.log("wys okna: ", window.innerHeight);
+    console.log("szer okna: ", window.innerWidth);
+    console.log("wielkosc jedzenia: ", this.props.foodSize);
+    console.log("wys score panelu: ", this.props.topScoreBoardHeight);
+
+  }
+
   render () {
     return (
       <div className="board">
@@ -17,6 +34,13 @@ class Board extends Component {
       </div>
     )
   }
+}
+
+// TODO: refactoring
+Board.defaultProps = {
+  foodSize: 50,
+  border: 10 * 2,
+  topScoreBoardHeight: 50
 }
 
 export default Board;
